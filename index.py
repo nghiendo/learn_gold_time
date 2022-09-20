9ff1d09a0893a3db989e63409694b59701a1d402
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2022-09-20 23:13:39 +0700

    HOTFIX LOGIN

--- index.py
+++ index.py
@@ -14,9 +14,9 @@ def blabla():
     token = request.cookies.get("_accessToken")
     if token is None or checkAuth(token) is False:
         return SignIn()
-app.before_request_funcs = {
-    None: [blabla]
-}
+# app.before_request_funcs = {
+#     None: [blabla]
+# }
 app.register_blueprint(courses_router, url_prefix="/courses")
 app.register_blueprint(question_router, url_prefix="/questions")
 app.register_blueprint(delete_router, url_prefix="/delete")
