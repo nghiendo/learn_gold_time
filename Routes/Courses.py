226a82fbb3c48344c2810381bf4ade071a5de572
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2022-09-20 22:43:51 +0700

    fix course

--- Routes/Courses.py
+++ Routes/Courses.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-# from Controllers.Courses import deleteCourse, index, insert, takeExam, viewCourse
+from Controllers.Courses import deleteCourse, index, insert, takeExam, viewCourse
 courses_router = Blueprint("courses_router", __name__)
-# courses_router.route("/", methods=['GET'])(index)
+courses_router.route("/", methods=['GET'])(index)
 # courses_router.route("/add", methods=['GET', 'POST'])(insert)
 # courses_router.route("/view/<id>", methods=['GET'])(viewCourse)
 # courses_router.route("/delete/<id>", methods=['GET'])(deleteCourse)
