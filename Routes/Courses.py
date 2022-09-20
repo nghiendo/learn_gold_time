20b45c6a0e0a1e5b2699b87b82a1b4e80a7c6ab1
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2022-09-20 22:45:14 +0700

    viewCourse

--- Routes/Courses.py
+++ Routes/Courses.py
@@ -4,6 +4,6 @@ from Controllers.Courses import deleteCourse, index, insert, takeExam, viewCours
 courses_router = Blueprint("courses_router", __name__)
 courses_router.route("/", methods=['GET'])(index)
 courses_router.route("/add", methods=['GET', 'POST'])(insert)
-# courses_router.route("/view/<id>", methods=['GET'])(viewCourse)
+courses_router.route("/view/<id>", methods=['GET'])(viewCourse)
 # courses_router.route("/delete/<id>", methods=['GET'])(deleteCourse)
 # courses_router.route("/exam/<id>", methods=['GET', 'POST'])(takeExam)
