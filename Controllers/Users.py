33321fccadf6db9ab966b54fd7f59d21821be193
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2022-09-23 15:19:19 +0700

    Check Json auth

--- Controllers/Users.py
+++ Controllers/Users.py
@@ -1,6 +1,5 @@
 from hashlib import md5
-from time import time
-from flask import request, redirect, render_template, session, url_for, globals
+from flask import request, redirect, render_template, session, url_for, globals, jsonify
 from Helper.database import Database
 from Helper.helper import checkAuth, createToken, loadSite, setToken
 
@@ -18,10 +17,10 @@ def checkForm(req):
         "password": md5(password.encode('utf-8')).hexdigest()
     })
     if len(users) == 0:
-        return 0, md5(password.encode('utf-8')).hexdigest()
+        return 0, users
     session['auth'] = email
     globals.Token = createToken(email)
-    return 1, md5(password.encode('utf-8')).hexdigest()
+    return 1, users
 
 def SignIn():
     if checkAuth(request.cookies.get('_accessToken')):
@@ -30,3 +29,12 @@ def SignIn():
     if result == 1:
         return setToken()
     return loadSite("Login.html", "Sign In", status = result, data={"result":users})
+
+def checkLogin():
+    if request.method != "POST":
+        return redirect(url_for("users_router.SignIn"))
+    email = request.form['email']
+    password = request.form['password']
+    user = Database('Users').select({"email": email, "password": md5(password.encode('utf-8')).hexdigest()})
+
+    return jsonify(user)
--- Routes/Users.py
+++ Routes/Users.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-from Controllers.Users import SignIn
+from Controllers.Users import SignIn, checkLogin
 
 users_router = Blueprint("users_router", __name__)
 users_router.route("/", methods=['GET', 'POST'])(SignIn)
+users_router.route("/signin", methods=['GET', 'POST'])(checkLogin)
 # users_router.route("/signin", methods=['POST'])(SignIn)
